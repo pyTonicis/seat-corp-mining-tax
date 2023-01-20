7fa0a74c3f4c764513c1e00de743bd6b9aa628f2
pyTonicis <s.wagner@daselektron.de>
2023-01-20 21:29:25 +0100

    Test

--- src/resources/views/corpminingtaxhome.blade.php
+++ src/resources/views/corpminingtaxhome.blade.php
@@ -17,6 +17,8 @@
     </div>
 @stop
 @push('javascript')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
 
