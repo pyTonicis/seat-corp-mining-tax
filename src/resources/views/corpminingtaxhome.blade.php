2a073e4f682c40b5a243af091ebbaba7e5795a2d
pyTonicis <s.wagner@daselektron.de>
2023-01-20 22:01:03 +0100

    Test

--- src/resources/views/corpminingtaxhome.blade.php
+++ src/resources/views/corpminingtaxhome.blade.php
@@ -57,7 +57,7 @@
             };
 
         new Chart(
-            document.getElementById('x-chart'),
+            document.getElementById('canvas#x-chart'),
             config
         )});
     </script>
