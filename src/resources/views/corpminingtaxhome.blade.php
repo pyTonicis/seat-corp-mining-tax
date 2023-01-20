e7c7f80754ee6b1f315d010eea5720c3288d8219
pyTonicis <s.wagner@daselektron.de>
2023-01-20 21:41:26 +0100

    Test

--- src/resources/views/corpminingtaxhome.blade.php
+++ src/resources/views/corpminingtaxhome.blade.php
@@ -8,17 +8,12 @@
 
 @section('left')
     <div class="card">
-        <div class="card-header">
-            <h3>Corporation Mining Thieves</h3>
-        </div>
         <div class="card-body">
-            <canvas id="myChart" height="150"></canvas>
+            <canvas id="myChart"></canvas>
         </div>
     </div>
 @stop
 @push('javascript')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script type="text/javascript">
         $(document).ready(function(){
 
