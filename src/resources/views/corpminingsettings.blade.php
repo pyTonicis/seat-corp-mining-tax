62e6019c86e988694175b80158a1590efef2a739
pyTonicis <s.wagner@daselektron.de>
2023-01-24 09:03:17 +0100

    Include Tax Settings

--- src/resources/views/corpminingsettings.blade.php
+++ src/resources/views/corpminingsettings.blade.php
@@ -21,19 +21,19 @@
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="corpId">Corporation</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <select class="groupSearch form-control input-xs" name="corpId" id="corpId"></select>
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="ore-price-modify">Ore Refining Rate</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="ore-refining-rate" name="ore-refining-rate" type="number" class="form-control input-md" value="91" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="ore-valuation-price">Ore Valuation Price</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <select class="custom-select mr-sm-2" name="ore-valuation-price" id="ore-valuation-price">
                                     <option value="1">Ore Price</option>
                                     <option value="2">Mineral Price</option>
@@ -42,7 +42,7 @@
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="ore-price-provider">Price Provider</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <select class="custom-select mr-sm-2" name="ore-price-provider" id="ore-price-provider">
                                     <option value="1">Eve Market</option>
                                     <option value="2">Eve Praisal</option>
@@ -51,7 +51,7 @@
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="price-modifier">Price Modifier</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="price-modifier" name="price-modifier" type="number" class="form-control input-md" value="98" min="0" max="100">
                             </div>
                         </div>
@@ -61,11 +61,11 @@
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="contract-holder">Price Modifier</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="contract-holder" name="contract-holder" type="text" class="form-control input-md" value="DollarBoy">
                             </div>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-12">
                             <label class="col-md4 col-form-label" for="contract-exprie">Expries in</label>
                             <select class="custom-select mr-sm-2" name="contract-exprie" id="contract-exprie">
                                 <option value="1">1 Day</option>
@@ -74,6 +74,27 @@
                                 <option value="4">1 Week</option>
                                 <option value="5">4 Weeks</option>
                             </select>
+                        </div>
+                        <div class="box-body">
+                            <legend>Tax Selector</legend>
+                        </div>
+                        <div class="form-group-row">
+                            <label class="col-md4 col-form-label" for="taxes-moon">Tax Moon ORE</label>
+                            <div class="col-md-4">
+                                <input id="taxes-moon" name="taxes-moon" type="checkbox" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group-row">
+                            <label class="col-md4 col-form-label" for="taxes-corp-moon">Tax Corporation Moon ORE</label>
+                            <div class="col-md-4">
+                                <input id="taxes-corp-moon" name="taxes-corp-moon" type="checkbox" class="form-control">
+                            </div>
+                        </div>
+                        <div class="form-group-row">
+                            <label class="col-md4 col-form-label" for="taxes-ore">Tax ORE</label>
+                            <div class="col-md-4">
+                                <input id="taxes-ore" name="taxes-ore" type="checkbox" class="form-control">
+                            </div>
                         </div>
                     </div>
                 </form>
@@ -91,49 +112,49 @@
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="r64-rate">R64</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="r64-rate" name="r64-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="r32-rate">R32</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="r32-rate" name="r32-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="r16-rate">R16</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="r16-rate" name="r16-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="r8-rate">R8</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="r8-rate" name="r8-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="r4-rate">R4</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="r4-rate" name="r4-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="ice-rate">ICE</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="ice-rate" name="ice-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="ore-rate">ORE</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="ore-rate" name="ore-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
                         <div class="form-group-row">
                             <label class="col-md4 col-form-label" for="gas-rate">GAS</label>
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="gas-rate" name="gas-rate" type="number" class="form-control input-md" value="10" min="0" max="100">
                             </div>
                         </div>
