86a94c925016779712f8b858c426e8c38e79882c
pyTonicis <s.wagner@daselektron.de>
2023-03-16 16:49:54 +0100

    Added Tax Database

---
+++ src/database/migrations/2023_03_16_152356_corp_mining_tax.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CorpMiningTax extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('corp_mining_tax', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('character_id');
+            $table->integer('month');
+            $table->integer('year');
+            $table->integer('quantity');
+            $table->integer('volume');
+            $table->integer('price');
+            $table->integer('tax');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
