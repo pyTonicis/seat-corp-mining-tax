59f7a914fa2c5a2ff6b902ef20ae28ec4fe69e7e
pyTonicis <s.wagner@daselektron.de>
2023-03-22 16:17:01 +0100

    Fix Typo in BarChart

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -31,7 +31,7 @@ Route::group([
                 ->name('corpminingtax.home')
                 ->uses('CorpMiningOverviewController@getHome');
 
-            Route::get('/')
+            Route::get('/miningchart')
                 ->name('corpminingtax.mining.chart.data')
                 ->uses('CorpMiningOverviewController@getCharacterMiningBarChartData');
 
