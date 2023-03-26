6c6c7920d869d69012710011dbd1bb8474d26be6
pyTonicis <s.wagner@daselektron.de>
2023-03-26 19:38:15 +0200

    Fix Routes

--- src/resources/views/corpmoonmining.blade.php
+++ src/resources/views/corpmoonmining.blade.php
@@ -6,16 +6,14 @@
 <div class="row">
     <div class="col-md-4">
         <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">J-OAH2 P3M7 <span class="badge badge-primary">R8</span></h3>
-            </div>
             <div class="card-body">
+                <h5 class="card-title"><i class="fas fa-gem"></i> J-OAH2 P3M7 <span class="badge badge-primary">R8</span></h5>
+                <h6><i class="fas fa-map"></i> J-OAH2</h6>
                 <table>
                     <thead>
                     <tr>
                         <td>Date</td>
                         <td>Volume</td>
-                        <td>OreTypes</td>
                         <td>Mineral Price</td>
                     </tr>
                     </thead>
@@ -23,8 +21,31 @@
                     <tr>
                         <td>2023-01-15</td>
                         <td>15.204.101 m³</td>
-                        <td>Lavish Chromite, Chromite, Bitumens</td>
                         <td>1.872.223.051 ISK</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title"><i class="fas fa-gem"></i> J-OAH2 P1M3 <span class="badge badge-primary">R8</span></h5>
+                <h6><i class="fas fa-map"></i> J-OAH2</h6>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>Date</td>
+                        <td>Volume</td>
+                        <td>Mineral Price</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>2023-02-02</td>
+                        <td>8.007.821 m³</td>
+                        <td>672.223.051 ISK</td>
                     </tr>
                     </tbody>
                 </table>
