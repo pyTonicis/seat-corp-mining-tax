41896a9f42c852998c8f41270872e7e6f2125faa
pyTonicis <s.wagner@daselektron.de>
2023-03-26 19:45:29 +0200

    Fix Routes

--- src/resources/views/corpmoonmining.blade.php
+++ src/resources/views/corpmoonmining.blade.php
@@ -7,7 +7,7 @@
     <div class="col-md-4">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title"><i class="fas fa-gem"></i> J-OAH2 P3M7 <span class="badge badge-primary">R8</span></h5>
+                <h5 class="card-title"><i class="fas fa-gem"></i> J-OAH2 P3M7 <span style="float:right" class="badge bg-success">R16</span></h5><br>
                 <h6><i class="fas fa-map"></i> J-OAH2</h6>
                 <table>
                     <thead>
@@ -31,8 +31,32 @@
     <div class="col-md-4">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title"><i class="fas fa-gem"></i> J-OAH2 P1M3 <span class="badge badge-primary">R8</span></h5>
+                <h5 class="card-title"><i class="fas fa-gem"></i> J-OAH2 P1M3 <span style="float:right" class="badge badge-primary">R8</span></h5>
                 <h6><i class="fas fa-map"></i> J-OAH2</h6>
+                <table>
+                    <thead>
+                    <tr>
+                        <td>Date</td>
+                        <td>Volume</td>
+                        <td>Mineral Price</td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>2023-02-02</td>
+                        <td>8.007.821 m³</td>
+                        <td>672.223.051 ISK</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title"><i class="fas fa-gem"></i> 0PU2-R P1M1 <span style="float:right" class="badge bg-warning">R32</span></h5>
+                <h6><i class="fas fa-map"></i>0PU2-R</h6>
                 <table>
                     <thead>
                     <tr>
