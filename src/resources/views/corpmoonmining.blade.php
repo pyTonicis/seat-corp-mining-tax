d3c92380979c81b7fe0c7af658096e827a9db825
pyTonicis <s.wagner@daselektron.de>
2023-03-29 14:08:46 +0200

    Fix MoonMining Models

--- src/resources/views/corpmoonmining.blade.php
+++ src/resources/views/corpmoonmining.blade.php
@@ -20,7 +20,7 @@
                     </div>
                     <div class="ml-auto mr-2 align-right text-center align-centered">
                         <div class="row">
-                            <h4>Total Mined: <b>{{ number_format($d->observers[$d->observer_id]->get_total_quantity($d->observer_id)) }} m³</b></h4>
+                            <h4>Total Mined: <b> m³</b></h4>
                         </div>
                     </div>
                 </div>
