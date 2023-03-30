b7090f4bbf60b85680b2be14a2deb43f48d587c6
pyTonicis <s.wagner@daselektron.de>
2023-03-30 21:27:39 +0200

    Fix Dashboard Tax value

--- src/Http/Controllers/CorpMiningOverviewController.php
+++ src/Http/Controllers/CorpMiningOverviewController.php
@@ -63,6 +63,7 @@ class CorpMiningOverviewController extends Controller
         $grp_moon = array();
         $grp_abyssal = array();
         $tax_count = 0;
+        $tax_act = 0;
         foreach ($labels as $label) {
             $datum = strtotime($label);
             $month = (int)date('m', $datum);
@@ -80,9 +81,11 @@ class CorpMiningOverviewController extends Controller
                 $minings->add_price($result->price);
                 $minings->add_quantity($result->quantity);
                 $tax_count += (int)$result->tax;
+                $tax_act = (int)$result->tax;
             } else {
                 array_push($data, 0);
                 array_push($prices, 0);
+                $tax_act = 0;
             }
             DB::statement("SET SQL_MODE=''");
             $groups = DB::table('character_minings as cm')
@@ -119,15 +122,6 @@ class CorpMiningOverviewController extends Controller
             array_push($grp_ore, (int)$ore);
             array_push($grp_abyssal, (int)$abyssal);
         }
-        $tax_act = 0;
-        $result = DB::table('corp_mining_tax')
-            ->select('quantity', 'volume', 'price', 'tax')
-            ->where('main_character_id', '=', $character)
-            ->where('month', '=', date('m', strtotime(time())))
-            ->where('year', '=', date('Y', strtotime(time())))
-            ->first();
-        if(!is_null($result))
-            $tax_act = (int)$result->tax;
         $minings->volume_per_month = $data;
         $minings->price_per_month = $prices;
         $dataset = array(['label' => 'Ice', 'data' => $grp_ice, 'backgroundColor' => '#4dc9f6'],
