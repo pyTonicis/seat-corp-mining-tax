c3cae3dda0555cf01a1f54f66902e9ef8204b150
pyTonicis <s.wagner@daselektron.de>
2023-04-09 15:52:34 +0200

    Fix Modal

--- src/resources/views/corpminingtaxcontracts.blade.php
+++ src/resources/views/corpminingtaxcontracts.blade.php
@@ -68,7 +68,7 @@
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
