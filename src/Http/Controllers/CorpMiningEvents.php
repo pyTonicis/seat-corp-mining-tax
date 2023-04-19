8c0f8c7ee68d6b5590910a5af0d0b57cbd28c125
pyTonicis <s.wagner@daselektron.de>
2023-04-19 15:59:30 +0200

    Fix Layout's

--- src/Http/Controllers/CorpMiningEvents.php
+++ src/Http/Controllers/CorpMiningEvents.php
@@ -6,6 +6,7 @@ use Seat\Web\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\DB;
+use View;
 class CorpMiningEvents extends Controller
 {
     public function getHome()
