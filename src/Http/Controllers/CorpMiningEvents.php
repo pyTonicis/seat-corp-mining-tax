9cb574b6490bb58ffc0ff3cf5ced8d5318aed3b9
pyTonicis <s.wagner@daselektron.de>
2023-04-20 21:59:29 +0200

    Added Action Buttons (Events)

--- src/Http/Controllers/CorpMiningEvents.php
+++ src/Http/Controllers/CorpMiningEvents.php
@@ -26,7 +26,7 @@ class CorpMiningEvents extends Controller
     {
         $update = DB::table('corp_mining_tax_events')
             ->insert(['event_name' => $request->get('event'), 'event_start' => $request->get('start'),
-                'event_duration' => $request->get('duration'), 'event_status' => 1, 'event_tax' => $request->get('taxrate')]);
+                'event_duration' => $request->get('duration'), 'event_status' => 1, 'event_tax' => $request->get('taxrate'), 'event_stop' => '1999-12-31']);
         $events = DB::table('corp_mining_tax_events')
             ->get();
         return view('corpminingtax::corpminingevents', ['events' => $events]);
