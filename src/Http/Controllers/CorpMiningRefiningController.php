448fe8973aa60c1552077e4a3ac5aa932ae00aa8
pyTonicis <s.wagner@daselektron.de>
2023-04-24 11:23:58 +0200

    Add Priceprovider to Refining Tool

--- src/Http/Controllers/CorpMiningRefiningController.php
+++ src/Http/Controllers/CorpMiningRefiningController.php
@@ -2,6 +2,7 @@
 
 namespace pyTonicis\Seat\SeatCorpMiningTax\Http\Controllers;
 
+use pyTonicis\Seat\SeatCorpMiningTax\Helpers\EvePraisalHelper;
 use pyTonicis\Seat\SeatCorpMiningTax\Services\ItemParser;
 use pyTonicis\Seat\SeatCorpMiningTax\Services\Reprocessing;
 use Seat\Web\Http\Controllers\Controller;
@@ -38,16 +39,19 @@ class CorpMiningRefiningController extends Controller
             $raw = Reprocessing::ReprocessOreByTypeId($item['typeID'], $item['quantity'], ((float)$request->get('modifier') / 100));
             foreach($raw as $n => $value) {
                 $inv_type = InvType::where('typeId', '=', $n)->first();
-                $price = Price::where('type_id', '=', $n)->first();
+                if ($request->get('provider') == 'Eve Market')
+                    $price = Price::where('type_id', '=', $n)->first()->average_price;
+                else
+                    $price = EvePraisalHelper::getItemPriceByTypeId($n);
                 if (!array_key_exists($inv_type->typeName, $refinedMaterials)) {
                     $refinedMaterials[$inv_type->typeName]['name'] = $inv_type->typeName;
                     $refinedMaterials[$inv_type->typeName]['typeID'] = $n;
                     $refinedMaterials[$inv_type->typeName]['quantity'] = $value;
-                    $refinedMaterials[$inv_type->typeName]['price'] = $price->average_price;
-                    $summary += (int)$price->average_price * (int)$value;
+                    $refinedMaterials[$inv_type->typeName]['price'] = $price;
+                    $summary += (int)$price * (int)$value;
                 } else {
                     $refinedMaterials[$inv_type->typeName]['quantity'] += $value;
-                    $summary += (int)$price->average_price * (int)$value;
+                    $summary += (int)$price * (int)$value;
                 }
             }
         }
