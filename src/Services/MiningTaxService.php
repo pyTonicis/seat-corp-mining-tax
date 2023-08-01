347c788acc96aaf50a093ae0641eac245204265f
pyTonicis <s.wagner@daselektron.de>
2023-08-01 07:48:53 +0200

    Fix Issue in Event Calculation

--- src/Services/MiningTaxService.php
+++ src/Services/MiningTaxService.php
@@ -201,8 +201,8 @@ class MiningTaxService
 
         foreach ($miningEventResult->characterData as $eventData) {
             if ($miningResult->hasCharacterData($eventData->characterId)) {
-                $charData = $miningResult->getCharacterDataById($eventData->characterId);
-                /*if ($charData->hasCharacterMining($eventData->type_id)) {
+
+                if ($miningResult->characterData[$eventData->characterId]->hasCharacterMining($eventData->type_id)) {
                     foreach (Reprocessing::ReprocessOreByTypeId($eventData->type_id, $eventData->quantity, (float)($eventData->event_tax / 100)) as $key => $value) {
 
                         if ($settings['ore_valuation_price'] == 'Ore Price') {
@@ -219,8 +219,6 @@ class MiningTaxService
                         $miningResult->characterData[$eventData->characterId]->delTax($price);
                     }
                 }
-            }*/
-                $miningResult->characterData[$eventData->characterId]->delTax(10000000);
             }
         }
         return $miningResult;
